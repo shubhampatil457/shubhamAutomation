fd6cbb4d16f35932a1c5c64a559f8385d417171b
shubham <patilshubham35@gmail.com>
2025-03-02 23:37:25 +0530

    first commit

--- eclipse-workspace/AutomationPracticeProg/src/arraydemo/SingleDimention.java
+++ eclipse-workspace/AutomationPracticeProg/src/arraydemo/SingleDimention.java
@@ -3,14 +3,15 @@ package arraydemo;
 public class SingleDimention {
 
 	public static void main(String[] args) {
-		int[] ar = { 8, 10, 3, 8, 7, 5, 23, 67, 45, 4, 7, 9, 10, 11, 34, 56, 89, 35, 19, 8, 3, 9, 2 };
+		int[] ar = { 16, 10, 3, 8, 7, 5, 23, 67, 45, 4, 7, 9, 10, 11, 34, 56, 89, 35, 19, 8, 3, 9, 2 };
 
 		int[] a = new int[3];
 		a[0] = 1;
 		a[1] = 4;
 		a[2] = 6;
+		
 
-		/*
+		/* i have made changes today for git
 		 * System.out.println(ar[0]); System.out.println(ar[3]);
 		 * System.out.println(ar[5]);
 		 */
